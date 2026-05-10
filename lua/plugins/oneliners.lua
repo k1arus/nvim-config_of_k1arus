708920decebf53acad52efbcbd150c12f6fb9785
k1arus <NjjHxdyDigdAynRxEPUT4wWVFuQfPSbxw7jnZSzE@proton.me>
2026-05-10 18:23:18 +0900

    Delete some files which is no longer needed.

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,7 +1,7 @@
 return {
---            { -- This helps with SSH tunneling and copying to clipboard
---                        'ojroques/vim-oscyank',
---                  },
+    --            { -- This helps with SSH tunneling and copying to clipboard
+        --                        'ojroques/vim-oscyank',
+        --                  },
         {
             -- Git plugin
             'tpope/vim-fugitive',
@@ -12,5 +12,12 @@ return {
             config = function()
                 require('nvim-highlight-colors').setup({})
             end
+        },
+        {
+            vim.keymap.set("n", "<leader>r", function()
+                local file = vim.fn.expand("%:p")
+                local out = "/tmp/qr_out"
+                vim.cmd("split | term gcc " .. file .. " -o " .. out .. " && " .. out)
+            end, { desc = "Run C file" })
         },
     }
